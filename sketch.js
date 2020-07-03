318417e939b09d96c73dd09edd1e6f133f876115
Parth Shah <helloparthshah@gmail.com>
2020-07-03 14:45:33 +0530

    Added sky and ground

--- sketch.js
+++ sketch.js
@@ -28,6 +28,7 @@ function setup() {
     walls.push(new Wall(0, sceneH, 0, 0));
     particle = new Particle(fov);
     particle.update(sceneW / 2, sceneH / 2);
+
 }
 
 function mousePressed() {
@@ -50,34 +51,43 @@ function mouseReleased() {
 
 function draw() {
 
+
     if (keyIsDown(65)) particle.rotate(-0.1); //left
     if (keyIsDown(68)) particle.rotate(0.1); //right
     if (keyIsDown(87)) particle.move(4); //up
     if (keyIsDown(83)) particle.move(-4); //down
 
     background(0);
+    // clear();
     for (let wall of walls)
         wall.show();
     // particle.update(mouseX, mouseY);
     particle.show();
 
+
     ellipse(mouseX, mouseY, 8);
     stroke(150);
     push();
     line(sx, sy, fx, fy);
+
+    fill(135, 206, 235);
+    rect(sceneW, 0, sceneW, sceneH / 2);
+
+    fill(100);
+    rect(sceneW, sceneH / 2, sceneW, sceneH / 2);
+
     pop();
 
     const scene = particle.look(walls);
     const w = sceneW / scene.length;
 
+
     push();
     translate(sceneW, 0);
-
 
     for (let i = 0; i < scene.length; i++) {
         // noStroke();
         const b = map(scene[i] * scene[i], 0, sceneW * sceneW, 255, 0);
-        // const h = map(scene[i], 0, sceneW, sceneH, 0);
         const h = sceneH * fov / scene[i];
         fill(0, 0, b);
         stroke(0, 0, b);
@@ -85,5 +95,6 @@ function draw() {
         rect(i * w + w / 2, sceneH / 2, w, h);
         // rect(i * w + w / 2, sceneH / 2, w, 100 * h / scene[i]);
     }
+
     pop();
 }
